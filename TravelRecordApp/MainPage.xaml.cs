9e0e2a8d536724094d39d81cab5047aca61387af
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-04-12 10:25:59 +0800

    created HomePage.xaml and added navigation function

--- TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/MainPage.xaml.cs
@@ -17,7 +17,17 @@ namespace TravelRecordApp
 
         void LoginButton_Clicked(System.Object sender, System.EventArgs e)
         {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
 
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
         }
     }
 }
